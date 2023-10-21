bc8f971539e763b467f24898f8da789a46e76385
Kiet <082.hoangtuankiet@gmail.com>
2023-10-21 22:01:42 +0700

    nop bai tap b35

---
+++ day-35/js/script.js
@@ -0,0 +1,48 @@
+let currentPage = 1;
+let isFetching = false;
+let hasMore = true;
+
+let root = document.querySelector(".home-page");
+
+const serverAPI = `http://localhost:3000`;
+
+const fetchData = async function () {
+  const stripHTML = (html) => html.replaceAll(/(<([^>]+)>)/gi, "");
+  isFetching = true;
+
+  let response = await fetch(`${serverAPI}/posts?_page=${currentPage}`);
+
+  let data = await response.json();
+
+  isFetching = false;
+
+  if (data.length === 0) {
+    hasMore = false;
+    return;
+  }
+
+  for (let post of data) {
+    let div = document.createElement("div");
+    div.innerHTML = `<h2 style="padding-left: 400px">${stripHTML(
+      post.title
+    )}</h2>
+    <img src=${post.image} style="padding-left: 400px"/>
+    <p style="padding-left: 400px">${stripHTML(post.excerpt)}</p>`;
+    root.appendChild(div);
+  }
+
+  currentPage++;
+};
+
+window.addEventListener("scroll", () => {
+  if (isFetching || !hasMore) {
+    return;
+  }
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    setTimeout(() => {
+      fetchData();
+    }, 1000);
+  }
+});
+
+fetchData();
