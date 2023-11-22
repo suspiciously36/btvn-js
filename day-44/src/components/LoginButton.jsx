2eceb3abc2c01d178a41c354b823ca608f3630ad
Kiet <082.hoangtuankiet@gmail.com>
2023-11-22 13:58:27 +0700

    fix popup login

--- day-44/src/components/LoginButton.jsx
+++ day-44/src/components/LoginButton.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithPopup, isAuthenticated } = useAuth0();
   return (
     !isAuthenticated && (
       <button
         className="sign-in"
         onClick={() => {
-          loginWithRedirect();
+          loginWithPopup();
         }}
       >
         ĐĂNG NHẬP || ĐĂNG KÝ
