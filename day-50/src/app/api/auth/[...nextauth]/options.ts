5ed7f17d64fd20eba87d9d615e3bbe6947d210a9
Kiet <082.hoangtuankiet@gmail.com>
2023-12-15 17:50:57 +0700

    as string hehe

--- day-50/src/app/api/auth/[...nextauth]/options.ts
+++ day-50/src/app/api/auth/[...nextauth]/options.ts
@@ -3,7 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const options: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET as string,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
